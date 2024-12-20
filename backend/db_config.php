0006aaa56127d09ae75785d32c3deded17c7e0a4
Tran Nguyen Tien Thanh <fortconquer909@gmail.com>
2024-12-20 23:08:33 +0700

    .

--- backend/db_config.php
+++ backend/db_config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "simplelogin";
+$dbname = "lamp_users";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
